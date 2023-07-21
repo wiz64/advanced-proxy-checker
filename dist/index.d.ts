00d16c96e0d0fb6f8ae0633482ace037267ad1e8
Mulayam S <mullayam06@gmail.com>
2023-07-21 22:18:42 +0530

    fixed ts errors/ dex/map files

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import TestFunctions from './connect';
+export default class ProxyClient extends TestFunctions {
+    full(proxy: string): Promise<any>;
+    getScore(proxy: string): Promise<any>;
+}
+//# sourceMappingURL=index.d.ts.map
